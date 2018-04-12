df589a42178c02e128562113cc0557e8f62cfeb5
Zhang Matt <mzhang@lcs.on.ca>
2018-04-12 14:10:09 -0400

    ITS DONE

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -10,8 +10,37 @@ import Foundation
 
 // Get the user input
 var rawInput = readLine()
+var sumOfFactors = 0
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+guard let input = rawInput else {
+    //error
+    print("input numbers please")
+    exit(9)
+}
 
+guard let integerInput = Int(rawInput!) else {
+    exit(9)
+}
+
+if integerInput == 1 {
+    print("Deficient Number")
+    exit(9)
+}
+
+for i in 1...integerInput-1 {
+    if integerInput%i == 0 {
+        sumOfFactors += i
+    }
+}
+
+print("The sum of factors is \(sumOfFactors)")
+
+if integerInput < sumOfFactors {
+    print("Abundant")
+}
+else if integerInput > sumOfFactors {
+    print("Deficient")
+}
+else if internalScrapErr == sumOfFactors {
+    print("Perfect")
+}

7cafcf1dfc8a58f10cb692fca92fa35f2b5dbf23
Zhang Matt <mzhang@lcs.on.ca>
2018-05-04 09:42:28 -0400

    done

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  RotatingLetters
-//
-//  Created by Gordon, Russell on 2018-04-06.
-//  Copyright © 2018 Gordon, Russell. All rights reserved.
-//
-
 import Foundation
 
 // Get the user input
